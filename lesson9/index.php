b04e31f421aa84bd0c18e589b7af94fa7f6c982f
screamdotv <krik.vladislv@gmail.com>
2023-11-01 20:33:09 +0200

    moving the class to a separate file, adding dependencies to the dockerfile.

--- lesson9/index.php
+++ lesson9/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Persone.php';
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -9,45 +10,6 @@ $encoders = new JsonEncoder();
 $normalizers = new ObjectNormalizer();
 
 $serializer = new Serializer([$normalizers], [$encoders]);
-
-class Person 
-{
-    public string $name;
-    public int $age;
-    public string $sex;
-    
-    // Геттер для свойства "name"
-    public function getName(): string {
-        return $this->name;
-    }
-
-    // Сеттер для свойства "name"
-    public function setName(string $name): void {
-        $this->name = $name;
-    }
-
-    // Геттер для свойства "age"
-    public function getAge(): int {
-        return $this->age;
-    }
-
-    // Сеттер для свойства "age"
-    public function setAge(int $age): void {
-        $this->age = $age;
-    }
-
-    // Геттер для свойства "sex"
-    public function getSex(): string {
-        return $this->sex;
-    }
-
-    // Сеттер для свойства "sex"
-    public function setSex(string $sex): void {
-        $this->sex = $sex;
-    }
-    
-    
-}
 
 $person = new Person();
 $person->setName("Vlad");
